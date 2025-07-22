5e924f698e4d0d4af940719a04d4aba9f05d966f
Andrew Chen <109437047+AxC1271@users.noreply.github.com>
2025-07-22 10:12:59 -0700

    Update instruction_fetch.vhd

--- rtl/instruction_fetch.vhd
+++ rtl/instruction_fetch.vhd
@@ -3,16 +3,17 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity InstructionFetch is
-    Port ( clk : in STD_LOGIC;
-           reset : in STD_LOGIC;
-           pc : in STD_LOGIC_VECTOR(31 downto 0);  -- Input from program counter
-           instruction : out STD_LOGIC_VECTOR(31 downto 0));
+    Port ( 
+        clk : in STD_LOGIC;
+        reset : in STD_LOGIC;
+        pc : in STD_LOGIC_VECTOR(31 downto 0);  -- input from program counter
+        instruction : out STD_LOGIC_VECTOR(31 downto 0));
 end InstructionFetch;
 
 architecture Behavioral of InstructionFetch is
     type memory_array is array (0 to 255) of STD_LOGIC_VECTOR(31 downto 0);
     constant instruction_memory : memory_array := (
-        0 => x"00400093",  -- Example instruction
+        0 => x"00400093",  -- example instruction
         1 => x"00800113",
         2 => x"00A00193",
         others => (others => '0')
