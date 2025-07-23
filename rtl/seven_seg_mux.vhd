d0dc754bed213365b0576bb8dae76d79451a1687
Andrew Chen <109437047+AxC1271@users.noreply.github.com>
2025-07-23 12:12:59 -0700

    Update seven_seg_mux.vhd

--- rtl/seven_seg_mux.vhd
+++ rtl/seven_seg_mux.vhd
@@ -6,6 +6,11 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity seven_seg_mux is
   port (
+    clk : in STD_LOGIC;
+    rst : in STD_LOGIC;
+    value : in STD_LOGIC_VECTOR(31 downto 0);
+    segment : out STD_LOGIC_VECTOR(6 downto 0);
+    anode : out STD_LOGIC_VECTOR(3 downto 0)
   );
 end seven_seg_mux;
 
