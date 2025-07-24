d5acd68dfc5be591d33594938139792c107696e1
Andrew Chen <109437047+AxC1271@users.noreply.github.com>
2025-07-24 10:36:38 -0700

    Update register_file_tb.vhd

--- RegisterFile/register_file_tb.vhd
+++ RegisterFile/register_file_tb.vhd
@@ -26,7 +26,7 @@ architecture Behavioral of register_file_tb is
   
   -- define intermediary signals here
   signal read_addr1, read_addr2, write_addr : STD_LOGIC_VECTO(4 downto 0);
-  signal reg-write : STD_LOGIC;
+  signal reg-write : STD_LOGIC := '0'; -- initialize it low
   signal write_data : STD_LOGIC_VECTO(31 downto 0);
   signal read_data1, read_data2 : STD_LOGIC_VECTO(31 downto 0);
 
@@ -47,6 +47,35 @@ begin
 
   stimulus: process
   begin
+    -- first apply reset
+    rst <= '1';
+    wait for 10 ns;
+    rst <= '0';
+    wait for 10 ns;
+
+    -- registers are now all set to zeroes so we don't have undefined values
+
+    -- test case #1: write to reg 1
+    write_addr <= "00001";     -- write to reg one
+    write_data <= x"00000001"; -- hexadecimal for 1
+    reg_write <= '1'; -- write enable for register 1
+    wait for 20 ns;
+    -- test case #2: write to reg 2
+    reg_write <= '0'; -- make sure to reset reg_write so we don't make changes to reg
+    write_addr <= "00010";     -- write to reg two
+    write_data <= x"00FF89D5"; -- arbitrary value
+    reg_write <= '1'; -- now we are ready to modify reg 2
+    wait for 20 ns;
+
+    -- test case #3: write to reg 0 which should be a default zero register
+    reg_write <= '0'; -- reset reg_write again
+    write_addr <= "00000";     -- write to reg zero
+    write_data <= x"01010101"; -- arbitrary value
+    reg_write <= '1'; -- now we are ready to modify reg 0
+    wait for 20 ns;
+    -- now we can read from both of these registers to see if they hold the values
+    
+    
   end process stimulus;
 end Behavioral;
   
