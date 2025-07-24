5bbc9837dd532373d02caf4b97188c92a31f8e80
Andrew Chen <109437047+AxC1271@users.noreply.github.com>
2025-07-24 11:28:47 -0700

    Update instruction_memory.vhd

--- InstructionMemory/instruction_memory.vhd
+++ InstructionMemory/instruction_memory.vhd
@@ -2,15 +2,15 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
-entity InstructionFetch is
+entity instruction_memory is
     port ( 
         clk : in STD_LOGIC;
         rst : in STD_LOGIC;
         pc : in STD_LOGIC_VECTOR(31 downto 0);  -- input from program counter
         instruction : out STD_LOGIC_VECTOR(31 downto 0));
-end InstructionFetch;
+end instruction_memory;
 
-architecture Behavioral of InstructionFetch is
+architecture Behavioral of instruction_memory is
     type memory_array is array (0 to 255) of STD_LOGIC_VECTOR(31 downto 0);
     constant instruction_memory : memory_array := (
         0 => x"00400093",  -- example instructions
@@ -25,7 +25,7 @@ begin
         if rst = '1' then
             instruction <= (others => '0');
         elsif rising_edge(clk) then
-            -- Use the program counter to fetch the instruction
+            -- use the program counter to fetch the instruction
             -- since the pc is incrementing by 4, exclude the last 2 bits
             -- so that we're effectively adding by 1 as indices
             instruction <= instruction_memory(to_integer(unsigned(pc)));
