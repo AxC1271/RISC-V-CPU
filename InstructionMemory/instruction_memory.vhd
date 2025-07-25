4844f4557dffae448599267b3893ebaf9647ea27
Andrew Chen <109437047+AxC1271@users.noreply.github.com>
2025-07-25 14:41:09 -0700

    Update instruction_memory.vhd

--- InstructionMemory/instruction_memory.vhd
+++ InstructionMemory/instruction_memory.vhd
@@ -4,10 +4,9 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity instruction_memory is
     port ( 
-        clk : in STD_LOGIC;
-        rst : in STD_LOGIC;
         pc : in STD_LOGIC_VECTOR(31 downto 0);  -- input from program counter
-        instruction : out STD_LOGIC_VECTOR(31 downto 0));
+        instruction : out STD_LOGIC_VECTOR(31 downto 0)
+    );
 end instruction_memory;
 
 architecture Behavioral of instruction_memory is
@@ -20,15 +19,5 @@ architecture Behavioral of instruction_memory is
     );
 
 begin
-    process(clk, rst)
-    begin
-        if rst = '1' then
-            instruction <= (others => '0');
-        elsif rising_edge(clk) then
-            -- use the program counter to fetch the instruction
-            -- since the pc is incrementing by 4, exclude the last 2 bits
-            -- so that we're effectively adding by 1 as indices
-            instruction <= instruction_memory(to_integer(unsigned(pc)));
-        end if;
-    end process;
+    instruction <= instruction_memory(to_integer(unsigned(pc(31 downto 0))));
 end Behavioral;
