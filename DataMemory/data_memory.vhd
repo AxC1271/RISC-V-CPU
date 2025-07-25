dc81db2a4bb1db1b3eab34d9a5b6cfa784481416
Andrew Chen <109437047+AxC1271@users.noreply.github.com>
2025-07-25 09:41:45 -0700

    Update data_memory.vhd

--- DataMemory/data_memory.vhd
+++ DataMemory/data_memory.vhd
@@ -1,1 +1,18 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
+entity data_memory is
+  port (
+    addr     : in STD_LOGIC_VECTOR(4 downto 0);
+    wr_data  : in STD_LOGIC_VECTOR(31 downto 0);
+    MemWrite : in STD_LOGIC;
+    MemRead  : in STD_LOGIC;
+    rd_data  : out STD_LOGIC_VECTOR(31 downto 0)
+  );
+end data_memory;
+
+architecture Behavioral of data_memory is
+begin
+  
+end Behavioral;
