b860351589e678664983bd139228adceeae86e83
Andrew Chen <109437047+AxC1271@users.noreply.github.com>
2025-07-25 10:44:08 -0700

    Update program_counter.vhd

--- ProgramCounter/program_counter.vhd
+++ ProgramCounter/program_counter.vhd
@@ -9,7 +9,6 @@ entity program_counter is
     port (
         clk : in STD_LOGIC;
         rst : in STD_LOGIC;
-        pc_write : in STD_LOGIC;                    -- enable PC update (for pipeline stalls)
         pc_src : in STD_LOGIC_VECTOR(31 downto 0);  -- new PC value from branch/jump
         branch_taken : in STD_LOGIC;                -- signal indicating branch/jump taken
         pc : out STD_LOGIC_VECTOR(31 downto 0)      -- current PC value
@@ -27,15 +26,7 @@ begin
         if rst = '1' then
             curr_pc <= RESET_PC;
         elsif rising_edge(clk) then
-            if pc_write = '1' then
-                if branch_taken = '1' then
-                    -- branch or jump taken - overwrite existing PC
-                    curr_pc <= pc_src;
-                else
-                    -- next sequential instruction
-                    curr_pc <= STD_LOGIC_VECTOR(unsigned(curr_pc) + 1);
-                end if;
-            end if;
+                curr_pc <= pc_src;
             -- if pc_write = '0', PC stays the same
         end if;
     end process;
