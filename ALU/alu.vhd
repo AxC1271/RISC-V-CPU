c3af99b452814bc24789b75dc7cdb87935c5e5b1
Andrew Chen <109437047+AxC1271@users.noreply.github.com>
2025-07-29 08:08:27 -0700

    Update alu.vhd

--- ALU/alu.vhd
+++ ALU/alu.vhd
@@ -21,38 +21,37 @@ begin
       case opcode is
          when "000" => -- ADD
             res_i <= std_logic_vector(unsigned(op1) + unsigned(op2));
-            
          when "001" => -- SUB
             res_i <= std_logic_vector(unsigned(op1) - unsigned(op2));
-            
          when "010" => -- AND
             res_i <= op1 and op2;
-            
          when "011" => -- OR
             res_i <= op1 or op2;
-            
          when "100" => -- XOR
             res_i <= op1 xor op2;
-            
          when "101" => -- SLT
             if unsigned(op1) < unsigned(op2) then
-               res_i <= (0 => '1', others => '0');  -- Result = 1
+               res_i <= (0 => '1', others => '0');
             else
-               res_i <= (others => '0');            -- Result = 0
+               res_i <= (others => '0');
             end if;
-            
          when "110" => -- SLL 
             res_i <= std_logic_vector(shift_left(unsigned(op1), to_integer(unsigned(op2(4 downto 0)))));
-            
          when "111" => -- SRL 
             res_i <= std_logic_vector(shift_right(unsigned(op1), to_integer(unsigned(op2(4 downto 0)))));
-            
-         when others => 
+         when others =>
             res_i <= (others => '0');
       end case;
    end process evaluation;
    
-   res <= res_i;
-   zero_flag <= '1' when res_i = (others => '0') else '0';
+   zero_flag_gen: process(res_i)
+   begin
+      if res_i = x"00000000" then
+         zero_flag <= '1';
+      else
+         zero_flag <= '0';
+      end if;
+   end process zero_flag_gen;
    
+   res <= res_i;
 end Behavioral;
